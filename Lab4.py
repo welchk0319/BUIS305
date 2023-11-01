a05f4571998c04e7b2aa32c5f127fff50165b159
welchk0319 <welchk0319@students.bowiestate.edu>
2023-11-01 14:00:39 -0400

    Lab4

---
+++ Lab4.py
@@ -0,0 +1,15 @@
+import math
+
+for i in range(10):
+    print(i)
+for i in range(1,10):
+    print(i)
+for i in range(1,10,2):
+    print(i)
+radius=float(input('Enter Radius'))
+area=math.pi*radius*radius
+print(area)
+Length=float(input('Enter Length of Rectangle'))
+Width=float(input('Enter Width of Rectangle'))
+area2=Length*Width
+print(area2)
